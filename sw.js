f6c77386db18fe46807fa35724306ac81f27e6fb
Mariana Macias Arevalo <marianamaciasarevalo@Marianas-MacBook-Pro.local>
2026-07-02 12:52:51 +0400

    Initial: Marmaroph members area PWA

---
+++ sw.js
@@ -0,0 +1,15 @@
+var CACHE = 'marmaroph-v1';
+var URLS = ['/', '/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
