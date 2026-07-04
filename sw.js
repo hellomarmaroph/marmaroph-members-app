59cab2297531d65bf1032d0fa354b3caa4c18a3c
Mariana Macias Arevalo <marianamaciasarevalo@Marianas-MacBook-Pro.local>
2026-07-04 21:23:41 +0400

    Optimize icons, fix service worker caching strategy

--- sw.js
+++ sw.js
@@ -1,14 +1,29 @@
-var CACHE = 'marmaroph-v2';
-var URLS = ['/marmaroph-members-app/', '/marmaroph-members-app/index.html'];
+var CACHE = 'marmaroph-v3';
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
   self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    }).then(function() { return self.clients.claim(); })
+  );
 });
 
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    fetch(e.request).catch(function() {
+    fetch(e.request).then(function(res) {
+      if (res.ok) {
+        var clone = res.clone();
+        caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+      }
+      return res;
+    }).catch(function() {
       return caches.match(e.request);
     })
   );
